dd1f7a8887007c825903a94f137862002e273843
kothapally15 <kothapally.15@wright.edu>
2025-05-14 13:16:13 -0400

    Added Basic Calculator operation

--- src/Calci.java
+++ src/Calci.java
@@ -3,7 +3,51 @@ import java.util.*;
 public class Calci{
 public static void main(String [] args){
 
-    System.err.println("New Project");
+    Scanner sc= new Scanner(System.in);
+
+    System.err.println("New Project Calci");
+    double x,y;
+    System.out.println("Enter the two numbers");
+    x= sc.nextDouble();
+    y=sc.nextDouble();
+    System.out.println("Enter the operation you want to calculate (+ , * , - ,/)");
+    char op= sc.next().charAt(0);
+    double val;
+    switch (op) {
+        case '+':
+            val=x+y;
+             System.out.println("The Result is "+val);
+            
+            break;
+        case '*':
+            val=x*y;
+             System.out.println("The Result is "+val);
+            
+            break;
+        case '-':
+            val=x-y;
+             System.out.println("The Result is "+val);
+            
+            break;
+        case '/':
+            if (y!=0) {
+                val=x/y;
+                System.out.println("The Result is "+val);
+            }
+            else{
+                System.out.println("Error divided by zero");
+            }
+            
+            
+            break;
+    
+        default:
+            System.out.println("Enter the correct operator");
+            break;
+    }
+
+
+
 
 }
 }
